25a7186dcbcec546a4777af5906ccdb45900ceb9
Cookie755233 <85786140+Cookie755233@users.noreply.github.com>
2025-01-23 18:31:33 +0800

    fix package.json again

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'your-vercel-frontend-url'
+    'https://your-frontend-domain.vercel.app'
   ],
   credentials: true
 }));
